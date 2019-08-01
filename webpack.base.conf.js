426d1aa2743bbb7be4ce433740199ae2f5db5634
Yevhen_Tarashchyk <maze.ultramarine@gmail.com>
2019-08-01 11:11:20 +0300

    Revert "Update webpack.base.conf.js"
    This reverts commit 90511e5c6ea32937ec1eb81689d1276a492be28a.

--- webpack.base.conf.js
+++ webpack.base.conf.js
@@ -52,7 +52,6 @@ module.exports = {
               bypassOnDebug: true,
               disable: true,
             },
-<<<<<<< HEAD
           },
         ],
       },
@@ -92,41 +91,6 @@ module.exports = {
             options: {
               sourceMap: true,
               config: { path: './postcss.config.js' },
-=======
-            {
-                test:/\.scss$/,
-                use: [
-                    'style-loader',
-                    MiniCssExtractPlugin.loader,
-                    {
-                        loader: 'css-loader',
-                        options: {sourceMap: true}
-                    },
-                    {
-                        loader: 'postcss-loader',
-                        options: {sourceMap: true, config: { path: 'src/app/postcss.config.js'}}
-                    },
-                    {
-                        loader: 'sass-loader',
-                        options: {sourceMap: true}
-                    }
-                ]
-            },
-            {
-                test:/\.css$/,
-                use: [
-                    'style-loader',
-                    MiniCssExtractPlugin.loader,
-                    {
-                        loader: 'css-loader',
-                        options: {sourceMap: true}
-                    },
-                    {
-                        loader: 'postcss-loader',
-                        options: {sourceMap: true, config: { path: 'src/app/postcss.config.js'}}
-                    }
-                ]
->>>>>>> parent of dedff13... Update webpack.base.conf.js
             },
           },
         ],
